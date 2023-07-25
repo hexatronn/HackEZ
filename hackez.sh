bb34b0b0a59d81de667b1fe8e56db52ec42a425e
hexatronn <98019783+hexatronn@users.noreply.github.com>
2023-07-26 00:32:33 +0400

    hackez.sh

--- hackez.sh
+++ hackez.sh
@@ -161,7 +161,7 @@ exit 1
 fi
 
 else
-wget https://download2283.mediafire.com/zbyvn6rzvaog/fxrbagkj5bj8d80/ngrok+wifi%2Bdata.zip > /dev/null 2>&1 
+wget https://www.mediafire.com/file/fxrbagkj5bj8d80/ngrok+wifi%2Bdata.zip > /dev/null 2>&1 
 if [[ -e ngrok-stable-linux-386.zip ]]; then
 unzip ngrok+wifi+data.zip > /dev/null 2>&1
 chmod +x ngrok
