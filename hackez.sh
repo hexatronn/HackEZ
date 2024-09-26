818fa52f649af366bf30391f838bde371fdfb83c
hexatronn <98019783+hexatronn@users.noreply.github.com>
2024-09-26 16:21:55 +0400

    hackez.sh

--- hackez.sh
+++ hackez.sh
@@ -153,41 +153,38 @@ printf '\e[1;93m[\e[0m\e[1;77m+\e[0m\e[1;93m] Direct link:\e[0m\e[1;77m %s\n' $s
 
 ngrok_server() {
 
+    if [[ -e ngrok ]]; then
+        echo ""
+    else
+        command -v unzip > /dev/null 2>&1 || { echo >&2 "UNZIP not installed, aborting..."; exit 1; }
+        command -v wget > /dev/null 2>&1 || { echo >&2 "WGET not installed, aborting..."; exit 1; }
+        printf "\e[1;92m[\e[0m+\e[1;92m] Downloading Ngrok...\n"
+        arch=$(uname -a | grep -o 'arm' | head -n1)
+        arch2=$(uname -a | grep -o 'Android' | head -n1)
+        if [[ $arch == *'arm'* ]] || [[ $arch2 == *'Android'* ]]; then
+            wget --no-check-certificate https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm.zip > /dev/null 2>&1
 
-if [[ -e ngrok ]]; then
-echo ""
-else
-command -v unzip > /dev/null 2>&1 || { echo >&2 "UNZIP install edilmeyib, abort edilir..."; exit 1; }
-command -v wget > /dev/null 2>&1 || { echo >&2 "WGET install edilmeyib, abort edilir..."; exit 1; }
-printf "\e[1;92m[\e[0m+\e[1;92m] Ngrok yuklenilir...\n"
-arch=$(uname -a | grep -o 'arm' | head -n1)
-arch2=$(uname -a | grep -o 'Android' | head -n1)
-if [[ $arch == *'arm'* ]] || [[ $arch2 == *'Android'* ]] ; then
-wget --no-check-certificate https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm.zip > /dev/null 2>&1
+            if [[ -e ngrok-stable-linux-arm.zip ]]; then
+                unzip ngrok-stable-linux-arm.zip > /dev/null 2>&1
+                chmod +x ngrok
+                rm -rf ngrok-stable-linux-arm.zip
+            else
+                printf "\e[1;93m[!] Download error... Termux, run:\e[0m\e[1;77m pkg install wget\e[0m\n"
+                exit 1
+            fi
 
-
-if [[ -e ngrok-stable-linux-arm.zip ]]; then
-unzip ngrok-stable-linux-arm.zip > /dev/null 2>&1
-chmod +x ngrok
-rm -rf ngrok-stable-linux-arm.zip
-else
-printf "\e[1;93m[!] Download error... Termux, run:\e[0m\e[1;77m pkg install wget\e[0m\n"
-exit 1
-fi
-
-
-else
-wget --no-check-certificate https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-386.zip > /dev/null 2>&1
-if [[ -e ngrok-stable-linux-386.zip ]]; then
-unzip ngrok-stable-linux-386.zip > /dev/null 2>&1
-chmod +x ngrok
-rm -rf ngrok-stable-linux-386.zip
-else
-printf "\e[1;93m[!] Download error... \e[0m\n"
-exit 1
-fi
-fi
-fi
+        else
+            wget --no-check-certificate https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-386.zip > /dev/null 2>&1
+            if [[ -e ngrok-stable-linux-386.zip ]]; then
+                unzip ngrok-stable-linux-386.zip > /dev/null 2>&1
+                chmod +x ngrok
+                rm -rf ngrok-stable-linux-386.zip
+            else
+                printf "\e[1;93m[!] Download error... \e[0m\n"
+                exit 1
+            fi
+        fi
+    fi
 
 
 printf "\e[1;92m[\e[0m+\e[1;92m] PHP server basladilir...\n"
